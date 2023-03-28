03c51c2e7a4919c0a2967291f0804886447daff9
Johnny L. de Alba <johnnyldealba@gmail.com>
2023-03-27 17:54:31 -0700

    Added public directory

---
+++ public/eccoserv/ecco-the-dolphin/sprite-sheets/index.php
@@ -0,0 +1,226 @@
+<?php
+
+define("WEB_PATH", "../../");
+
+define("PAGE_TITLE", "Ecco the Dolphin - Sprite Sheets");
+define("PAGE_DESCRIPTION", "A collection of sprite sheets from the Mega Drive version of Ecco the Dolphin.");
+define("PAGE_DIR", "/ecco-the-dolphin/sprite-sheets");
+
+include(WEB_PATH . "header.php");
+
+?>
+
+    <img src="card.png" class="panel_header" alt="ecco the dolphin sprite sheets" />
+
+<div class='dialog'>
+    <div class='dialog_caption'>Ecco the Dolphin, Sprite Sheets</div>
+
+<div class='document_body'>
+<div class="width_30 rfloat_l">
+    <div class="exhibit">
+        <img src="../title-screen.png" alt="ecco the dolphin" />
+        <div class="exhibit_caption_b">Ecco the Dolphin</div>
+    </div>
+</div>
+<div class="width_70 rfloat_l">
+    <div class="summary">
+
+<p>A collection of sprite sheets from Ecco the Dolphin for the Sega Genesis/Mega Drive. These sprite sheets were extracted from the game itself using tools created from our reverse engineering efforts. The tools are capable of decoding and reading Mega Drive bitmaps, and various Ecco the Dolphin sprite and level formats. Our sprite sheets are free to distribute as long as credit is given to <a href="https://eccothedolphin.online">https://eccothedolphin.online</a></p>
+
+<p class="bold underline">Download Sprite Sheets Collection</p>
+<ul><li><a href="../ecco-mega-drive-sprite-sheets.zip">ecco-mega-drive-sprite-sheets.zip</a> (1.5 MB)</li></ul>
+    </div>
+</div>
+</div>
+
+<div class="dialog_category">Sprite Sheets</div>
+<div class="align_c">
+
+<br />
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/alphabet.png" class="exhibit_caption">Alphabet/Font</a>
+    <a href="../sprite-sheets/alphabet.png">
+    <img src="../icons/_alphabit.png" alt="ecco alphabit font" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/artic-spider.png" class="exhibit_caption">Artic Spider</a>
+    <a href="../sprite-sheets/artic-spider.png">
+    <img src="../icons/_artic-spider.png" alt="ecco artic spider" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/bubbles.png" class="exhibit_caption">Bubbles</a>
+    <a href="../sprite-sheets/bubbles.png">
+    <img src="../icons/_bubbles.png" alt="ecco bubbles" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/crab.png" class="exhibit_caption">Crab</a>
+    <a href="../sprite-sheets/crab.png">
+    <img src="../icons/_crab.png" alt="ecco crab" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/dolphin.png" class="exhibit_caption">Dolphin</a>
+    <a href="../sprite-sheets/dolphin.png">
+    <img src="../icons/_dolphin.png" alt="ecco dolphin" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/dunkleosteus.png" class="exhibit_caption">Dunkleosteus</a>
+    <a href="../sprite-sheets/dunkleosteus.png">
+    <img src="../icons/_dunkleosteus.png" alt="ecco dunkleosteus" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/fish.png" class="exhibit_caption">Fish</a>
+    <a href="../sprite-sheets/fish.png">
+    <img src="../icons/_fish.png" alt="ecco fish" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/glyph.png" class="exhibit_caption">Glyph</a>
+    <a href="../sprite-sheets/glyph.png">
+    <img src="../icons/_glyph.png" alt="ecco glyph" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/jellyfish.png" class="exhibit_caption">Jellyfish</a>
+    <a href="../sprite-sheets/jellyfish.png">
+    <img src="../icons/_jellyfish.png" alt="ecco jellyfish" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/leech.png" class="exhibit_caption">Leech</a>
+    <a href="../sprite-sheets/leech.png">
+    <img src="../icons/_leech.png" alt="ecco leech" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/leopard-shark.png" class="exhibit_caption">Leopard Shark</a>
+    <a href="../sprite-sheets/leopard-shark.png">
+    <img src="../icons/_leopard-shark.png" alt="ecco leopard shark" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/objects-and-obstacles.png" class="exhibit_caption">Objects and Obstacles</a>
+    <a href="../sprite-sheets/objects-and-obstacles.png">
+    <img src="../icons/_objects-and-obstacles.png" alt="ecco objects and obstacles" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/octopus.png" class="exhibit_caption">Octopus</a>
+    <a href="../sprite-sheets/octopus.png">
+    <img src="../icons/_octopus.png" alt="ecco octopus" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/orca.png" class="exhibit_caption">Orca</a>
+    <a href="../sprite-sheets/orca.png">
+    <img src="../icons/_orca.png" alt="ecco orca" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/orthocone.png" class="exhibit_caption">Orthocone</a>
+    <a href="../sprite-sheets/orthocone.png">
+    <img src="../icons/_orthocone.png" alt="ecco orthocone" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/power-bar.png" class="exhibit_caption">Power Bar</a>
+    <a href="../sprite-sheets/power-bar.png">
+    <img src="../icons/_power-bar.png" alt="ecco power bar" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/prehistoric-jellyfish.png" class="exhibit_caption">Prehistoric Jellyfish</a>
+    <a href="../sprite-sheets/prehistoric-jellyfish.png">
+    <img src="../icons/_prehistoric-jellyfish.png" alt="ecco prehistoric jellyfish" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/pteranodon.png" class="exhibit_caption">Pteranodon</a>
+    <a href="../sprite-sheets/pteranodon.png">
+    <img src="../icons/_pteranodon.png" alt="ecco power bar" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/pufferfish.png" class="exhibit_caption">Pufferfish</a>
+    <a href="../sprite-sheets/pufferfish.png">
+    <img src="../icons/_pufferfish.png" alt="ecco pufferfish" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/seahorse.png" class="exhibit_caption">Seahorse</a>
+    <a href="../sprite-sheets/seahorse.png">
+    <img src="../icons/_seahorse.png" alt="ecco power bar" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/shark.png" class="exhibit_caption">Shark</a>
+    <a href="../sprite-sheets/shark.png">
+    <img src="../icons/_shark.png" alt="ecco shark" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/sonar.png" class="exhibit_caption">Sonar</a>
+    <a href="../sprite-sheets/sonar.png">
+    <img src="../icons/_sonar.png" alt="ecco sonar" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/sonar-responce.png" class="exhibit_caption">Sonar Responce</a>
+    <a href="../sprite-sheets/sonar-responce.png">
+    <img src="../icons/_sonar-responce.png" alt="ecco sonar responce teleport ring" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/spikes.png" class="exhibit_caption">Spikes</a>
+    <a href="../sprite-sheets/spikes.png">
+    <img src="../icons/_spikes.png" alt="ecco spikes" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/splash.png" class="exhibit_caption">Splash</a>
+    <a href="../sprite-sheets/splash.png">
+    <img src="../icons/_splash.png" alt="ecco splash" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/thunder.png" class="exhibit_caption">Thunder</a>
+    <a href="../sprite-sheets/thunder.png">
+    <img src="../icons/_thunder.png" alt="ecco thunder" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/trilobite.png" class="exhibit_caption">Trilobite</a>
+    <a href="../sprite-sheets/trilobite.png">
+    <img src="../icons/_trilobite.png" alt="ecco trilobite" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/unused-turtle.png" class="exhibit_caption">Unused Turtle</a>
+    <a href="../sprite-sheets/unused-turtle.png">
+    <img src="../icons/_unused-turtle.png" alt="ecco unused turtle" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/vortex-drone.png" class="exhibit_caption">Vortex Drone</a>
+    <a href="../sprite-sheets/vortex-drone.png">
+    <img src="../icons/_vortex-drone.png" alt="ecco vortex drone" /></a>
+</div>
+
+<div class="exhibit quarter">
+    <a href="../sprite-sheets/vortex-queen.png" class="exhibit_caption">Vortex Queen</a>
+    <a href="../sprite-sheets/vortex-queen.png">
+    <img src="../icons/_vortex-queen.png" alt="ecco vortex queen" /></a>
+</div>
+
+<br />&nbsp;
+
+</div>
+</div>
+
+<?php include(WEB_PATH . "footer.php"); ?>
